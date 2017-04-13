5d0fca3d9f42943b21eef3841a0a25343c4384b1
guilong li <guilong li>
2017-04-12 21:07:54 -0400

    Initial commit for our first spring boot project

---
+++ src/main/java/com/asymbuddy/AsymBuddyApplication.java
@@ -0,0 +1,12 @@
+package com.asymbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsymBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsymBuddyApplication.class, args);
+	}
+}
